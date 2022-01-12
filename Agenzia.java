112d30c708375e1764fa3f6886cac42892ecada7
filomenaci <cfcignarale@gmail.com>
2022-01-12 18:23:41 +0100

    Pulito il case 1, ho messo tutto quello che c'è da mettere nelle funzioni

--- Agenzia.java
+++ Agenzia.java
@@ -6,8 +6,10 @@ public class Agenzia {
 		
 		String menu = "1. Aggiungere annuncio\n2. Eliminare annuncio\n3. Elenco\n4. Visualizza dettagli\n5. Elenco sintetico\n6. Filtro prezzo\n7. Salva\n8. Carica\n9. Esci\n";
     String menu2 = "A. Aggiungi un appartamento.\nB. Aggiungi una villetta";
+    String menu3 = "1. Dotato di doppi servizi.\n 2. Non dotato di doppi servizi";
     int scelta = 0;
     String sceltaTipologia = "";
+    int sceltaServizi = 0;
     Vector <Annunci> Lista = new Vector <Annunci> ();
          
 
@@ -28,81 +30,6 @@ public class Agenzia {
             switch (scelta) {
               case 1: {
                 System.out.println ("Aggiungere annuncio");
-                 do {
-                      try {
-                          System.out.println(menu2);
-                          sceltaTipologia = input.next();
-                      }
-                      catch (InputMismatchException e) {
-                          System.out.println("Non è un");
-                          input.next();
-                          sceltaTipologia = "";
-                      }
-
-                  }
-                  while (sceltaTipologia.equals(""));  
-
-                  switch(sceltaTipologia){
-                    case "A":{
-                      System.out.println("Aggiungere infomazioni relative all'appartamento"); 
-                      
-                      System.out.println("Inserisci data: \n");
-                      int dat = input.nextInt();
-                      System.out.println("Inserisci ubicazione: \n");
-                      String ub = input.nextLine();
-                      System.out.println("Inserisci metratura: \n");
-                      int metri = input.nextInt();
-                      System.out.println("Inserisci anno di costruzione dell'immobile: \n");
-                      int anno = input.nextInput();
-                      System.out.println("Inserisci classe energetica: \n");
-                      String energia = input.nextLine();
-                      System.out.println("Inserisci il numero delle stanze: \n");
-                      int stanze = input.nextInt();
-                      //System.out.println("Inserisci se nell'immobile ci sono doppi servizi: \n"); 
-                      System.out.println("Inserisci prezzo dell'immobile: \n");
-                      int prez = input.nexInt();
-                      System.out.println("Inserisci il piano dell'appartamento: \n");
-                      int pianoA = input.nextInt();
-                      System.out.println("Inserisci il numero dei piani presenti nel condominio: \n");
-                      int pianoB= input.nextInt ();
-                      //System.out.println("Inserisci se l'immobile è dotato di garage: \n");
-                      //System.out.println("Inserisci se l'immobile ha il riscaldamento autonomo: \n");
-
-                      Appartamento app = new Appartamento (dat, ub, metri, anno, energia, stanze, prez, pianoA, pianoB);
-                      Lista.add(app);
-                        
-                    }
-          
-                    case "B" :{
-                    	System.out.println("Aggiungere informazioni relative alla villetta");
-                      
-                      
-                      System.out.println("Inserisci data: \n");
-                      int dat = input.nextInt();
-                      System.out.println("Inserisci ubicazione: \n");
-                      String ub = input.nextLine();
-                      System.out.println("Inserisci metratura: \n");
-                      int metri = input.nextInt();
-                      System.out.println("Inserisci anno di costruzione dell'immobile: \n");
-                      int anno = input.nextInt();
-                      System.out.println("Inserisci classe energetica: \n");
-                      String energia = input.nextLine();
-                      System.out.println("Inserisci il numero delle stanze: \n");
-                      int stanze = input.nextInt();
-                      //System.out.println("Inserisci se nell'immobile ci sono doppi servizi: \n");
-                      System.out.println("Inserisci prezzo dell'immobile: \n");
-                      int prez = input.nexInt();
-                      System.out.println("Inserisci la metratura del giardino: \n");
-                      int mgiardino = input.nextInt ();
-
-                      Villetta vill = new Villetta (dat, ub, metri, anno, energia, stanze, prez, pianoA, pianoB, mgiardino);
-                      Lista.add(vill);
-                    }  default : {
-                    	input.close();
-                    	return;
-                    }
-                  } break;
-              }
               case 2: {
                 System.out.println ("Eliminare annuncio"); break;
               }
@@ -132,10 +59,109 @@ public class Agenzia {
               default: {
                 System.out.println ("Valore errato");
               }
-          }  
+            }  
         }
-	    }
-    }
+	    // da qui iniziamo a fare le funzioni, ho semplicemnte tagliato tutti gli switch  fatti in  Case 1
+         do {
+                      try {
+                          System.out.println(menu2);
+                          sceltaTipologia = input.next();
+                      }
+                      catch (InputMismatchException e) {
+                          System.out.println("Non è un");
+                          input.next();
+                          sceltaTipologia = "";
+                      }
+
+                  }
+                  while (sceltaTipologia.equals(""));  
+
+                  switch(sceltaTipologia){
+                    case "A":{
+                      System.out.println("Aggiungere infomazioni relative all'appartamento"); 
+                      
+                      System.out.println("Inserisci data: \n");
+                      int dat = input.nextInt();
+                      System.out.println("Inserisci ubicazione: \n");
+                      String ub = input.nextLine();
+                      System.out.println("Inserisci metratura: \n");
+                      int metri = input.nextInt();
+                      System.out.println("Inserisci anno di costruzione dell'immobile: \n");
+                      int anno = input.nextInt();
+                      System.out.println("Inserisci classe energetica: \n");
+                      String energia = input.nextLine();
+                      System.out.println("Inserisci il numero delle stanze: \n");
+                      int stanze = input.nextInt();
+                      System.out.println("Inserisci se nell'immobile ci sono doppi servizi: \n"); 
+                      
+        while (true) {
+            do {
+                try {
+                    System.out.println(menu3);
+                    scelta = input.nextInt();
+                }
+                catch (InputMismatchException e) {
+                    System.out.println("Non è un numero!");
+                    input.next();
+                    scelta = 0;
+                }
+            }
+            while (sceltaServizi == 0);
+                      switch (sceltaServizi) {
+                        case 1: {
+                          boolean servizi = true;
+                        }
+                        case 2 :{
+                          boolean servizi = false;
+                        }
+                      }
+
+                      System.out.println("Inserisci prezzo dell'immobile: \n");
+                      int prez = input.nextInt();
+                      System.out.println("Inserisci il piano dell'appartamento: \n");
+                      int pianoA = input.nextInt();
+                      System.out.println("Inserisci il numero dei piani presenti nel condominio: \n");
+                      int pianoB= input.nextInt ();
+                      //System.out.println("Inserisci se l'immobile è dotato di garage: \n");
+                      //System.out.println("Inserisci se l'immobile ha il riscaldamento autonomo: \n");
+
+                      Appartamenti app = new Appartamenti (dat, ub, metri, anno, energia, stanze, prez, pianoA, pianoB);
+                      Lista.add(app);
+                        
+                    }
+          
+                    case "B" :{
+                    	System.out.println("Aggiungere informazioni relative alla villetta");
+                      
+                      
+                      System.out.println("Inserisci data: \n");
+                      int dat = input.nextInt();
+                      System.out.println("Inserisci ubicazione: \n");
+                      String ub = input.nextLine();
+                      System.out.println("Inserisci metratura: \n");
+                      int metri = input.nextInt();
+                      System.out.println("Inserisci anno di costruzione dell'immobile: \n");
+                      int anno = input.nextInt();
+                      System.out.println("Inserisci classe energetica: \n");
+                      String energia = input.nextLine();
+                      System.out.println("Inserisci il numero delle stanze: \n");
+                      int stanze = input.nextInt();
+                      //System.out.println("Inserisci se nell'immobile ci sono doppi servizi: \n");
+                      System.out.println("Inserisci prezzo dell'immobile: \n");
+                      int prez = input.nextInt();
+                      System.out.println("Inserisci la metratura del giardino: \n");
+                      int mgiardino = input.nextInt ();
+
+                      Villette vill = new Villette (dat, ub, metri, anno, energia, stanze, prez, pianoA, pianoB, mgiardino);
+                      Lista.add(vill);
+                    }  default : {
+                    	input.close();
+                    	return;
+                    }
+                  } break;
+              }
+ 
+}
 
 
 
