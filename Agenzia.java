baa766b1b46db73574899a95ef0e1ccef3199336
filomenaci <cfcignarale@gmail.com>
2022-01-13 12:53:18 +0100

    inserimeto metodi

--- Agenzia.java
+++ Agenzia.java
@@ -58,10 +58,11 @@ public class Agenzia {
                 System.out.println ("Valore errato");
               }
             }  
+          }
         }
 
-
-        private static void aggiuntaAnnuncio(){
+//metodi ausiliari
+        private static <Annnunci> void aggiuntaAnnuncio(){
           Scanner input = new Scanner (System.in);
           String menu2 = "A. Aggiungi un appartamento.\nB. Aggiungi una villetta";
           String sceltaTipologia = "";
@@ -84,74 +85,36 @@ public class Agenzia {
                   switch(sceltaTipologia){
                     case "A":{
                       System.out.println("Aggiungere infomazioni relative all'appartamento");break; 
-                      //aggiuntaAppartamento();
-                    }
+                      Agenzia.aggiuntaAppartamento();
+                    } break;
                     case "B" :{
                     	System.out.println("Aggiungere informazioni relative alla villetta");break;
-                      //aggiuntaVilletta();
-                    }
+                      Agenzia.aggiuntaVilletta();
+                    } break;
                }
         }
+        
 
         private static void aggiuntaAppartamento(){
-          Scanner input = new Scanner (System.in)
-          String menu3 = "1. Dotato di doppi servizi.\n 2. Non dotato di doppi servizi";
-          int sceltaServizi = 0;
-          String dat = ""; 
-          String ub = "";
-          int metri = 0;
-          int anno = 0;
-          String energia = "";
-          int stanze = 0;
-          boolean servizi = true;
-          int prez = 0;
-          int pianoA = 0;
-          int pianoB = 0;
-          boolean doppS = true;
-          boolean garag = true;
-          boolean riscaldamentoA = true;
-          
-          System.out.println("Inserisci data: \n");
-          int dat = input.nextInt();
-          System.out.println("Inserisci ubicazione: \n");
-          String ub = input.nextLine();
-          System.out.println("Inserisci metratura: \n");
-          int metri = input.nextInt();
-          System.out.println("Inserisci anno di costruzione dell'immobile: \n");
-          int anno = input.nextInt();
-          System.out.println("Inserisci classe energetica: \n");
-          String energia = input.nextLine();
-          System.out.println("Inserisci il numero delle stanze: \n");
-          int stanze = input.nextInt();
-          System.out.println("Inserisci se nell'immobile ci sono doppi servizi: \n");
-          //sceltaServizi
-          System.out.println("Inserisci prezzo dell'immobile: \n");
-          int prez = input.nextInt();
-          System.out.println("Inserisci il piano dell'appartamento: \n");
-          int pianoA = input.nextInt();
-          System.out.println("Inserisci il numero dei piani presenti nel condominio: \n");
-          int pianoB = input.nextInt();
-          System.out.println("Inserisci se l'immobile è dotato di garage: \n");
-          //sceltaGarage
-          System.out.println("Inserisci se l'immobile ha il riscaldamento autonomo:\n");
-          //sceltaRiscaldamento
-          // \n");
-        }
-
-        private static void aggiuntaVilletta(){
           Scanner input = new Scanner (System.in);
-          String dat = "";
+          String data = ""; 
           String ub = "";
           int metri = 0;
           int anno = 0;
           String energia = "";
           int stanze = 0;
-          boolean servizi = true;
+          boolean doppiS = true;
           int prez = 0;
-          int mgiardino = 0;
+          String tipologia = "appartamento";
+          int pianoA = 0;
+          int pianoCond = 0;
+          boolean garage = true;
+          boolean riscaldamento = true;
+          
 
+          System.out.println("Inserimento annuncio di tipo" +tipologia);
           System.out.println("Inserisci data: \n");
-          dat = input.nextInt();
+          data = input.nextLine();
           System.out.println("Inserisci ubicazione: \n");
           ub = input.nextLine();
           System.out.println("Inserisci metratura: \n");
@@ -163,12 +126,175 @@ public class Agenzia {
           System.out.println("Inserisci il numero delle stanze: \n");
           stanze = input.nextInt();
           System.out.println("Inserisci se nell'immobile ci sono doppi servizi: \n");
-          //sceltaservizi
+          doppiS = Agenzia.sceltaServizi();
+          System.out.println("Inserisci prezzo dell'immobile: \n");
+          prez = input.nextInt();
+          System.out.println("Inserisci il piano dell'appartamento: \n");
+          pianoA = input.nextInt();
+          System.out.println("Inserisci il numero dei piani presenti nel condominio: \n");
+          pianoCond = input.nextInt();
+          System.out.println("Inserisci se l'immobile è dotato di garage: \n");
+          garage = Agenzia.sceltaGarage();
+          System.out.println("Inserisci se l'immobile ha il riscaldamento autonomo:\n");
+          riscaldamento = Agenzia.sceltaRiscaldamento();
+          // \n");
+        }
+
+        private static  void aggiuntaVilletta(){
+          Scanner input = new Scanner (System.in);
+          String data = "";
+          String ub = "";
+          int metri = 0;
+          int anno = 0;
+          String energia = "";
+          int stanze = 0;
+          boolean doppiS = true;
+          int prez = 0;
+          String tipologia = "Villetta"; 
+          int mgiardino = 0;
+
+          System.out.println("Inserimento annuncio di tipo" + tipologia);
+          System.out.println("Inserisci data: \n");
+          data = input.nextLine();
+          System.out.println("Inserisci ubicazione: \n");
+          ub = input.nextLine();
+          System.out.println("Inserisci metratura: \n");
+          metri = input.nextInt();
+          System.out.println("Inserisci anno di costruzione dell'immobile: \n");
+          anno = input.nextInt();
+          System.out.println("Inserisci classe energetica: \n");
+          energia = input.nextLine();
+          System.out.println("Inserisci il numero delle stanze: \n");
+          stanze = input.nextInt();
+          System.out.println("Inserisci se nell'immobile ci sono doppi servizi: \n");
+          doppiS = Agenzia.sceltaServizi();
           System.out.println("Inserisci prezzo dell'immobile: \n");
           prez = input.nextInt();
           System.out.println("Inserisci la metratura del giardino: \n");
           mgiardino = input.nextInt();
 
+        }
+
+        private static boolean sceltaServizi(){
+          Scanner input = new Scanner (System.in);
+
+          String menu3 = "1. Dotato di doppi servizi.\n 2. Non dotato di doppi servizi";
+          int sceltaServizi = 0;
+          boolean doppS = true;
+         
+          while (true) {
+            do {
+                try {
+                    System.out.println(menu3);
+                    sceltaServizi = input.nextInt();
+                }
+                catch (InputMismatchException e) {
+                    System.out.println("Non è un numero!");
+                    input.next();
+                    sceltaServizi = 0;
+                }
+            }
+            while (sceltaServizi == 0);
+                     
+            switch (sceltaServizi) {
+                        case 1: {
+                          doppS = true;
+                          return doppS;
+                        } break;
+                        case 2 :{
+                          doppS = false;
+                          return doppS;
+                        } break;
+                        case 3: {
+                          input.close();
+                        }
+                        default: {
+                          System.out.println("Valore errato");
+                        }
+                      }
+                    }
+        }   
+        
+
+
+        private static boolean sceltaGarage(){
+          Scanner input = new Scanner (System.in);
+
+          String menu4 = "1. Dotato di doppi garage.\n 2. Non dotato di garage";
+          int sceltaGarage = 0;
+          boolean garage = true;
+         
+          while (true) {
+            do {
+                try {
+                    System.out.println(menu4);
+                    sceltaGarage = input.nextInt();
+                }
+                catch (InputMismatchException e) {
+                    System.out.println("Non è un numero!");
+                    input.next();
+                    sceltaGarage = 0;
+                }
+            }
+            while (sceltaGarage == 0);
+                     
+            switch (sceltaGarage) {
+                        case 1: {
+                          garage = true;
+                          return garage;
+                        } break;
+                        case 2 :{
+                          garage = false;
+                          return garage;
+                        } break;
+                        case 3 :{
+                          input.close();
+                        }
+                        default: {
+                          System.out.println("Valore errato");
+                        }
+                      }
+                    }
+        }
+
+
+        private static boolean sceltaRiscaldamento(){
+          Scanner input = new Scanner (System.in);
+
+          String menu5 = "1. Dotato di riscaldamento.\n 2. Non dotato di riscaldamento";
+          int sceltaRis= 0;
+          boolean riscaldamento = true;
+         
+          while (true) {
+            do {
+                try {
+                    System.out.println(menu5);
+                    sceltaRis = input.nextInt();
+                }
+                catch (InputMismatchException e) {
+                    System.out.println("Non è un numero!");
+                    input.next();
+                    sceltaRis = 0;
+                }
+            }
+            while (sceltaRis == 0);
+                     
+            switch (sceltaRis) {
+                        case 1: {
+                          riscaldamento = true;
+                          return riscaldamento;
+                        } break;
+                        case 2 :{
+                          riscaldamento = false;
+                          return riscaldamento;
+                        } break;
+                        case 3 :{
+                          input.close();
+                        }
+                        default: {
+                          System.out.println("Valore errato");
+                        }
+                      }
         }
 
 	    // da qui iniziamo a fare le funzioni, ho semplicemnte tagliato tutti gli switch  fatti in  Case 1
